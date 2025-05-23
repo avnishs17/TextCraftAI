1d5c9c3b34d7f98a97d3f04842549669db4485eb
Avnish Singh <avnish1708@gmail.com>
2025-05-23 22:08:41 +0530

    Data transforamtion

---
+++ src/textCraftAI/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,14 @@
+from textCraftAI.config.configuration import ConfigurationManager
+from textCraftAI.conponents.data_transformation import DataTransformation
+from textCraftAI.logging import logger
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
